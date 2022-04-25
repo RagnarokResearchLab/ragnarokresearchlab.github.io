85a405622e038bba486d2928af2a0ec4408f00dd
RDW <duckwhale@mail.ru>
2022-04-25 09:54:42 +0200

    UI: Update the copyright notice text in the footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -107,7 +107,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Ragnarok Research Lab authors and contributors.<br/>All trademarks referenced herein are the properties of their respective owners.`,
       },
       colorMode: {
         disableSwitch: true,
