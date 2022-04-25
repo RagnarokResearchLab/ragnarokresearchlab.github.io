a913d3337feaa81a7f5dc7b10cefc9db6836afe4
RDW <duckwhale@mail.ru>
2022-04-25 09:50:29 +0200

    UI: Replace the default navigation bar logo with a custom one

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,10 +43,10 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'Ragnarok Research Lab',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'Ragnarok Research Lab Logo',
+          src: 'img/logo-with-more-space-around.png',
         },
         items: [
           {
