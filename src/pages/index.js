28a2661afb1915fdf00100c713264fe238de9121
RDW <duckwhale@mail.ru>
2023-02-20 04:15:47 +0100

    UI: Remove the tutorial button from the landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,6 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}></div>
       </div>
     </header>
   );
